6b0b01521876d35d8d6a031809ed6c8935500631
David Sun <david.sun@builtin.com>
2025-09-18 13:38:59 -0700

    some cfg files

---
+++ play-creator-frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+﻿module.exports = {
+  root: true,
+  env: { browser: true, es2023: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-refresh/recommended',
+    'prettier'
+  ],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+};
